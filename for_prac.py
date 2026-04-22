5940563f38b13277edfc173369d75772ff3720f6
Shanto93 <shantoislam7363@gmail.com>
2026-04-22 13:24:18 +0600

    for with range function

--- for_prac.py
+++ for_prac.py
@@ -2,9 +2,12 @@
 b = ["a", "b", "c", "d", "e"]
 c = ["red", "green", "blue", "yellow", "orange"]
 
-for i in c:
-    if i == "yellow":
-        for j in i:
-            print(j)
-        continue
+# for i in c:
+#     if i == "yellow":
+#         for j in i:
+#             print(j)
+#         continue
+#     print(i)
+
+for i in range(1,20,3):
     print(i)
